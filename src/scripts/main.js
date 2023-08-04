dc8e22f21f2c3d92e7bf2e99fc1f92caf4cc798f
Vladyslav Horovyi <vladyslav.horovyi.dev@gmail.com>
2023-08-04 17:42:01 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const menu = document.querySelector('#menu');
+const menu_open = document.getElementById('menu-open');
+const overlay = document.querySelector('.overlay');
+const close_menu = document.getElementById('close-menu');
+
+menu_open.addEventListener('click', () => {
+  menu.classList.add('menu-open');
+  overlay.classList.add('active');
+});
+
+close_menu.addEventListener('click', () => {
+  menu.classList.remove('menu-open');
+  overlay.classList.remove('active');
+});
