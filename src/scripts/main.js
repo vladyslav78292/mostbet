4ade17d563d8b2378caaceddad923143e107ed58
Vladyslav Horovyi <vladyslav.horovyi.dev@gmail.com>
2023-08-07 15:09:01 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+let counter = 0;
+
+for (const menu of document.querySelectorAll(".menu")) {
+  menu.addEventListener("click", () => {
+      menu.querySelector(".menu-text").classList.toggle("menu-text-open");
+      if (counter % 2 === 0) {
+        menu.classList.add("active");
+      } else {
+        menu.classList.remove("active");
+      }
+      counter = counter + 1;
+      menu.querySelector(".menu-content").classList.toggle(
+          "menu-content-hidden"
+      );
+      console.log(counter)
+  });
+}
